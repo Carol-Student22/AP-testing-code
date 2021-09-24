3cc7783fc6d2cdf17576102996f60be1c3739bcd
Carol-Student22 <carol.lian22@gmail.com>
2021-09-24 12:27:53 -0400

    .,

--- src/js/array.js
+++ src/js/array.js
@@ -1,11 +1,36 @@
-const myQuestions = [
-    {
-      class: "",
-      title: {
-        a: "",
-        b: "",
-        c: "",
-      },
-      correctAnswer: "",
-    },
-  ];
+const weatherChart = [
+  {
+    weather: "sunny",
+    case:"",
+  },
+  {
+    weather: "partial-cloudy",
+    case:"",
+  },
+  {
+    weather: "cloudy",
+    case:"",
+  },
+  {
+    weather: "fog",
+    case:"",
+  },
+  {
+    weather: "drizzle",
+    case:"",
+  },
+  {
+    weather: "rain",
+    case:"",
+  },
+  {
+    weather: "storm",
+    case:"",
+  },
+  {
+    weather: "hurricane",
+    case:"",
+  },
+];
+
+export {weatherChart};
