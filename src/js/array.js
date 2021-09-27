2754f66baf311ded1df074611dcd4cbaa65ba9d0
Carol-Student22 <Carol-Student22@users.noreply.github.com>
2021-09-26 20:06:34 -0400

    .

--- src/js/array.js
+++ src/js/array.js
@@ -1,36 +1,28 @@
 const weatherChart = [
   {
     weather: "sunny",
-    case:"",
   },
   {
     weather: "partial-cloudy",
-    case:"",
   },
   {
     weather: "cloudy",
-    case:"",
   },
   {
     weather: "fog",
-    case:"",
   },
   {
     weather: "drizzle",
-    case:"",
   },
   {
     weather: "rain",
-    case:"",
   },
   {
     weather: "storm",
-    case:"",
   },
   {
     weather: "hurricane",
-    case:"",
   },
 ];
 
-export {weatherChart};
+export { weatherChart };
