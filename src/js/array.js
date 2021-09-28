b9b2f731a3b242699b865f12bf5954e7370a38f8
Carol-Student22 <Carol-Student22@users.noreply.github.com>
2021-09-27 21:03:06 -0400

    changed.

--- src/js/array.js
+++ src/js/array.js
@@ -1,28 +1,75 @@
 const weatherChart = [
   {
     main: "sunny",
-    sub: "",
+    sub: "test",
+    choices: {
+      a: "test",
+      b: "test",
+    },
+    preferchoice: "",
   },
   {
     main: "partial-cloudy",
+    sub: "f",
+    choices: {
+      a: "f",
+      b: "f",
+    },
+    preferchoice: "",
   },
   {
     main: "cloudy",
+    sub: "f",
+    choices: {
+      a: "f",
+      b: "f",
+    },
+    preferchoice: "",
   },
   {
     main: "fog",
+    sub: "f",
+    choices: {
+      a: "f",
+      b: "f",
+    },
+    preferchoice: "",
   },
   {
     main: "drizzle",
+    sub: "f",
+    choices: {
+      a: "f",
+      b: "f",
+    },
+    preferchoice: "",
   },
   {
     main: "rain",
+    sub: "f",
+    choices: {
+      a: "f",
+      b: "f",
+    },
+    preferchoice: "",
   },
   {
     main: "storm",
+    sub: "f",
+    choices: {
+      a: "f",
+      b: "f",
+    },
+    preferchoice: "",
   },
   {
     main: "hurricane",
+    sub: "f",
+    choices: {
+      a: "f",
+      b: "f",
+    },
+    preferchoice: "",
   },
 ];
 
