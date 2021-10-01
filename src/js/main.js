6a2fa1427a91fdbdc574018f40b8aec16cdf2ed3
Carol-Student22 <carol.lian22@gmail.com>
2021-10-01 12:25:58 -0400

    .

--- src/js/main.js
+++ src/js/main.js
@@ -74,26 +74,34 @@ const cardArray = [
 ];
 
 
-function buildCard() {
+function grabArray() {
   //grab a random element from the array
   const random = cardArray[Math.floor(Math.random() * cardArray.length)];
   console.log(random);
   console.log("Grabbing random array complete");
+}
 
   //have the element appear on screen with the proper class and tags
-  const cardHTML = document.getElementsByClassName('card');
-  function stu() {cardHTML.insertAdjacentHTML('afterbegin', 
-    `<!-- <div class="text-container">
-      <h1 class="main-text">Sunny</h1>
-      <h2 class="sub-text">A bright lightbulb.</h2>
-    </div>`
-  );
-  console.log(cardHTML)}
+function cardHTML() {
+  const card = document.createElement("li");
+  card.innerHTML = `
+    <div class="text-container">
+        <h1 class="main-text">Sunny</h1>
+        <h2 class="sub-text">A bright lightbulb.</h2>
+    </div>
+    <div class="btn-container">
+        <button class="btn">Stare at it</button>
+        <button class="btn">Look Away</button>
+    </div>  
+
+  `;
+}
+
   //grab the button tags and stuff
 
   //check if the selected btn is the preferchoice of option
 
   //if it is the preferchoice then add to the answers correct, else don't add
-}
 
-buildCard();
+
+
